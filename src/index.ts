2bbf4e037c87e6055749d8cb1130c3ca451276f1
Yash Makkar <yash.mkr108@gmail.com>
2025-12-31 14:02:24 +0530

    huskey eslint prettier setup done

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, prefer-const
-let x = 1;
-
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    msg: "Hi there",
+    msg: 'Hi there',
   });
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
